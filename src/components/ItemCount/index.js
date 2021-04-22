ba7b84ce98c7672e4f048807eb3cfa3573d67871
Miranda Portocarrero <portocarreromiranda@gmail.com>
2021-04-22 20:15:24 -0300

    tercera parte desafio clase 5

--- src/components/ItemCount/index.js
+++ src/components/ItemCount/index.js
@@ -1,47 +1,11 @@
 import { useState } from 'react';
-import Navbar from '../Navbar';
 
 const ItemCount = () => {
-    const [products, setProducts]= useState(
-        {
-            name:'iphone 99',
-            madeBy:'Apple',
-            description:'Muy caro'
-        },
-
-        {
-            name:'1100',
-            madeBy:'Nokia',
-            description:'Durisimo!'
-        },
-        
-        {
-            name:'S9',
-            madeBy:'Samsung',
-            description:'Bueno'
-        },
-        
-        {
-            name:'G1',
-            madeBy:'Motorola',
-            description:'ultimo modelo'
-        },
-
-        {
-            name:'Note 10',
-            madeBy:'Xiaomi',
-            description:'Muy recomensable'
-        }
-        )
+    const [count, setCount] = useState(0);
 
     return ( 
         <div>
-            <Navbar products="products"/>
-            <div>
-                <div>
-                    <h4>{}</h4>
-                </div>
-            </div>
+            <button>click</button>
         </div>
     );
 }
--- src/components/ItemListContainer/index.js
+++ src/components/ItemListContainer/index.js
@@ -1,14 +1,59 @@
 import './style.css';
+import { useState } from 'react';
 
 const ItemListContainer = ({greeting}) => {
-    return( <>
-    <div className="fondo">
+    const [filter, setFilter] = useState('')
+
+    const [products, setProducts]= useState([
+        {
+            name:'iphone 99',
+            madeBy:'Apple',
+            description:'Muy caro'
+        },
+
+        {
+            name:'1100',
+            madeBy:'Nokia',
+            description:'Durisimo!'
+        },
         
-        <h1> {greeting}</h1>
+        {
+            name:'S9',
+            madeBy:'Samsung',
+            description:'Bueno'
+        },
+        
+        {
+            name:'G1',
+            madeBy:'Motorola',
+            description:'ultimo modelo'
+        },
+
+        {
+            name:'Note 10',
+            madeBy:'Xiaomi',
+            description:'Excelente!!'
+        }]
+        )
+
+    console.log('filter =>', filter);
+
+    return( <>
+        <div className="fondo">
+            <h1>{greeting}</h1>
             <p> $55000</p>
             <p> Muy buena para el hogar!</p>
         </div>
-      
+         
+        <input type='text' placeholder='Search Phone' value={filter} onChange={(e) => setFilter(e.target.value)} />
+        <div>
+            {products.map((product) =>(  
+                        <div className="bordesProductos">
+                            <p>{`${product.madeBy} ${product.name}`}</p>
+                            <p>{product.description}</p>
+                        </div>
+            ))}
+        </div>
         </>
     );
 }
