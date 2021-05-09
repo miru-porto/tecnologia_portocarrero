28f0186c2ba592d2416845ba5f972df0dd059ff7
Miranda Portocarrero <portocarreromiranda@gmail.com>
2021-05-09 00:34:10 -0300

    arreglando cart

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,10 +1,76 @@
+import "./styles.css";
+import { useContext } from "react";
+import { CartContext } from "../../context/cartContext";
 
 const Cart = () => {
+  const { cart } = useContext(CartContext);
   return (
     <>
-      <h1>Aquí figurarán todos los productos que agregaste a tu carrito!</h1>
+      <h1 className="titleOne">Mi compra</h1>
+      <div className="cart">
+        <table className="cartTable">
+          <tr className="cartTableborder">
+            <th className="cartTableTitleName">PRODUCTO</th>
+            <td className="cartTableTitleName">PRECIO</td>
+            <td className="cartTableTitleName">CANTIDAD</td>
+            <td className="cartTableTitleName">SUBTOTAL</td>
+          </tr>
+          {cart.map((p) => {
+            return (
+              <tr>
+                <th>{p.name}</th>
+                <td>ARS${p.price}</td>
+                <td>{p.qty}</td>
+                <td>SUBTOTAL</td>
+              </tr>
+            );
+          })}
+        </table>
+
+        <div className="cart_recibo">
+          <div className="cart_info">
+            <p>Resumen de compra</p>
+            <p>Total: ARS$ XXX</p>
+          </div>
+          <button>Finalizar compra</button>
+        </div>
+      </div>
     </>
   );
 };
+
+/*
+<h1 className="cart_title">Mi compra</h1>
+      <div className="cart">
+        <div className="cart_components">
+          <div className="cart_subtitles">
+            <p>Producto</p>
+            <p>Precio</p>
+            <p>Cantidad</p>
+            <p>Subtotal</p>
+          </div>
+          <div className="cartProductList">
+            {cart.map((p) => {
+              return (
+                <div className="cartProductItem">
+                  <p className="cartProductName">{p.name}</p>
+                  <p>{p.price}</p>
+                  <p>{p.qty}</p>
+                  <p>SUBTOTAL</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="cart_recibo">
+          <div className="cart_info">
+            <p>Resumen de compra</p>
+            <p>Total: ARS$ XXX</p>
+          </div>
+          <button>Finalizar compra</button>
+        </div>
+      </div>
+
+*/
 
 export default Cart;
--- src/components/CartWidget/index.js
+++ src/components/CartWidget/index.js
@@ -1,14 +1,33 @@
 import "./styles.css";
+import { useContext } from "react";
+import { CartContext } from "../../context/cartContext";
 
-const CartWidget = (product) => {
-  return <div className="cartWidgetContainer">
-    <img className="img" src="../images/carro-de-la-compra.png" />
-    <div className="modal">
-      <p>Producto</p>
-      <p>Cantidad</p>
-      <img src={product.image}/>
+const CartWidget = () => {
+  const { cart } = useContext(CartContext);
+
+  return (
+    <div className="cartWidgetContainer">
+      <img className="img" src="../images/carro-de-la-compra.png" />
+      <div className="modal">
+        <div className="title">
+          <p>Producto</p>
+          <p>Cantidad</p>
+        </div>
+        {cart.map((p) => {
+          return (
+            <>
+              <div className="infoFather">
+                <div className="infoSon">
+                  <p>{p.name}</p>
+                  <p>{p.qty}</p>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default CartWidget;
