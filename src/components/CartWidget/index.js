ccf9b8a622437affcb65a519adb16b7b3a1c9229
Miranda Portocarrero <portocarreromiranda@gmail.com>
2021-05-31 10:52:34 -0300

    mejorando cartwidget

--- src/components/CartWidget/index.js
+++ src/components/CartWidget/index.js
@@ -8,39 +8,41 @@ const CartWidget = () => {
 
   return (
     <>
-    <div className="cartWidgetContainer">
-      <Link to={"/cart"}>
-        <img className="img" src="../images/agregar.png" />
-      </Link>
-      <div className="modal">
-        
-        {cart.length > 0 ? (
-        <>  
-        <table className="infoContainerTable">
-          <td className="infoTable">Producto</td>
-          <td className="infoTable">Cantidad</td>
-        </table>
-         
-        {cart.map((p) => {
-            return (
-              <>
-                <div className="infoFather">
-                  <table className="infoSon">
-                    <td className="infoTable">{p.name}</td>
-                    <td className="infoTable">{p.qty}</td>
-                  </table>
-                </div>
-              </>
-            );
-          })}
-          </>
-        ) : (
-          <>
-            <p className="infoNoProducts">Aún no hay productos</p>
-          </>
-        )}
+      <div className="cartWidgetContainer">
+        <Link to={"/cart"}>
+          <img
+            className="cartWidgetContainer_img"
+            src="../images/agregar.png"
+          />
+        </Link>
+        <div className="modal">
+          {cart.length > 0 ? (
+            <>
+              <table className="infoContainerTable">
+                <td className="infoTable">Producto</td>
+                <td className="infoTable">Cantidad</td>
+              </table>
+
+              {cart.map((p) => {
+                return (
+                  <>
+                    <div>
+                      <table>
+                        <td className="infoTable">{p.name}</td>
+                        <td className="infoTable">{p.qty}</td>
+                      </table>
+                    </div>
+                  </>
+                );
+              })}
+            </>
+          ) : (
+            <>
+              <p className="infoNoProducts">Aún no hay productos</p>
+            </>
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
 };
