22c1bd2a29d7c00b0ccac17ba83d94a2e8554e0d
Miranda Portocarrero <portocarreromiranda@gmail.com>
2021-05-31 12:08:01 -0300

    cambiando color loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 const LoaderComponent = () => {
   return <div className="loaderContainer">
-      <Loader type="ThreeDots" color="#e4e4e4" height={80} width={80} />
+      <Loader type="ThreeDots" color="#e0c2de" height={80} width={80} />
   </div>
 }
 
