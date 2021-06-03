904644205c8e9eb02a9601276cc5c01ff67e4adc
Miranda Portocarrero <portocarreromiranda@gmail.com>
2021-06-03 16:30:44 -0300

    quitando bs

--- src/components/ItemList/index.js
+++ src/components/ItemList/index.js
@@ -1,14 +1,15 @@
 import Item from "../Item";
-import { Container } from "react-bootstrap";
 import "./styles.css";
 
 const ItemList = ({ products }) => {
   return (
-    <Container fluid="bg" className="cards-container">
-      {products.map((p) => (
-        <Item product={p} key={p.id} />
-      ))}
-    </Container>
+    <>
+      <div className="itemList">
+        {products.map((p) => (
+          <Item product={p} key={p.id} />
+        ))}
+      </div>
+    </>
   );
 };
 
